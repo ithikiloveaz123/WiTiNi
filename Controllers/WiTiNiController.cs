3eb48ed3b849790a2a405da5dfc08394bca2a577
ithikiloveaz123 <thuanphanhoangminh4566@gmail.com>
2024-04-10 15:26:07 +0700

    Add project files.

---
+++ Controllers/WiTiNiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WiTiNi.Controllers
+{
+    public class WiTiNiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
